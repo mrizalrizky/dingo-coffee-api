d0969b0eb3f28a85fb90449298e6b44d29928503
mrizalrizky <61452014+mrizalrizky@users.noreply.github.com>
2024-04-08 06:21:38 +0700

    add new origin

--- app/config/allowedOrigins.config.js
+++ app/config/allowedOrigins.config.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
-    'http://localhost:5173',
-    'http://192.168.1.6:5173'
-]
+  "http://localhost:5173",
+  "http://192.168.1.6:5173",
+  "https://dev-rizal.d3vevb2mv9xg6.amplifyapp.com",
+];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
